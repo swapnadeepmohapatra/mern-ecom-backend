b2a1b3c4c639b3e2560dc185598d3dd848d70439
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-03-25 23:37:29 +0530

    added encryption to user password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
+const uuidv1 = require("uuid/v1");
+
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -23,10 +26,9 @@ const UserSchema = new Schema({
     type: String,
     trim: true
   },
-  //   TODO: Comeback here
-  password: {
+  encry_password: {
     type: String,
-    trim: true
+    required: true
   },
   salt: String,
   role: {
@@ -36,6 +38,35 @@ const UserSchema = new Schema({
   purchases: {
     type: Array,
     default: []
+  }
+});
+
+UserSchema.virtual("password")
+  .set(function(password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.encry_password = this.securePassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
+UserSchema.method({
+  securePassword: function(plainPassword) {
+    if (!plainPassword) return "";
+    try {
+      const hash = crypto
+        .createHmac("sha256", this.salt)
+        .update(plainPassword)
+        .digest("hex");
+      return hash;
+    } catch (error) {
+      return "";
+    }
+  },
+
+  authenticate: function(plainPassword) {
+    return this.securePassword(plainPassword) === this.encry_password;
   }
 });
 
