03c479707784eaf9a1e33815845dc040a714b6c6
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-03-26 21:26:02 +0530

    added validation in user signup

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const { check } = require("express-validator");
 const { signout, signup } = require("../controllers/auth");
 
 const router = express.Router();
 
-router.post("/signup", signup);
+router.post(
+  "/signup",
+  [
+    check("name")
+      .isLength({ min: 3 })
+      .withMessage("name should be at least 3 character long"),
+    check("email")
+      .isEmail()
+      .withMessage("valid email is required"),
+    check("password")
+      .isLength({ min: 5 })
+      .withMessage("password should be of minimum length 5")
+  ],
+  signup
+);
 router.get("/signout", signout);
 
 module.exports = router;
