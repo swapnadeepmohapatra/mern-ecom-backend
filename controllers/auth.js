f9c2bc48947c462fefe06e181e2dac826756197b
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-03-26 21:10:03 +0530

    added signup method

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,21 @@
-exports.signout = (req, res) => {
-  res.json({ user: "done" });
-};
+const User = require("../models/user");
 
 exports.signup = (req, res) => {
+  const user = new User(req.body);
+  user.save((error, user) => {
+    if (error) {
+      return res.status(400).json({
+        error: error.errmsg
+      });
+    }
+    res.json({
+      name: user.name,
+      email: user.email,
+      id: user._id
+    });
+  });
+};
+
+exports.signout = (req, res) => {
   res.json({ message: "Signup" });
 };
