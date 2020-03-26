853169862aae24f83c354de820c03189223f5440
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-03-26 11:10:22 +0530

    added product schema

---
+++ models/product.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxLength: 32
+    },
+    descripiton: {
+      type: String,
+      trim: true,
+      required: true,
+      maxLength: 2000
+    },
+    price: {
+      type: Number,
+      required: true,
+      maxlength: 32,
+      trim: true
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+      required: true
+    },
+    stock: {
+      type: Number
+    },
+    sold: {
+      type: Number,
+      default: 0
+    },
+    photo: {
+      data: Buffer,
+      contentType: String
+    }
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", ProductSchema);
+
+module.exports = Product;
