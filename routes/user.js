f60309bb607d239cbc7838f7797c3aacd9a3e0fe
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-03-27 23:39:07 +0530

    added userPurchaseList route

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUserById, getUser, updateUser } = require("../controllers/user");
+const {
+  getUserById,
+  getUser,
+  updateUser,
+  userPurchaseList
+} = require("../controllers/user");
 const { isAdmin, isAuthenticated, isSignedIn } = require("../controllers/auth");
 
 router.param("userId", getUserById);
 
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
 router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
+router.get(
+  "/orders/user/:userId",
+  isSignedIn,
+  isAuthenticated,
+  userPurchaseList
+);
 
 module.exports = router;
