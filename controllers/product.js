6d6774abd3a557d9507c49c73a109e0f83f8b938
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-03-28 23:33:39 +0530

    added createProduct route

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,7 @@
 const Product = require("../models/product");
+const formidable = require("formidable");
+const _ = require("lodash");
+const fs = require("fs");
 
 exports.getProductById = (req, res, next, id) => {
   Product.findById(id)
@@ -12,4 +15,37 @@ exports.getProductById = (req, res, next, id) => {
       req.product = product;
       next();
     });
+};
+
+exports.createProduct = (req, res) => {
+  let form = formidable.IncomingForm();
+  form.keepExtension = true;
+  form.parse(req, (error, fields, file) => {
+    if (error) {
+      res.status(400).json({
+        error: "problem with image"
+      });
+    }
+    // TODO: restrictions in the fields
+    let product = new Product(fields);
+
+    if (file.photo) {
+      if (file.photo.size > 3000000) {
+        return res.status(400).json({
+          error: "file size too big"
+        });
+      }
+      product.photo.data = fs.readFileSync(file.photo.path);
+      product.photo.contentType = file.photo.type;
+    }
+
+    product.save((error, product) => {
+      if (error) {
+        return res.status(400).json({
+          error: "Saving product failed"
+        });
+      }
+      res.json(product);
+    });
+  });
 };
