e2f0f60b0e44b1ed8e8bdad7587c167cb4fbb3b0
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-03-28 10:43:58 +0530

    added category route

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
 
 mongoose
   .connect(process.env.DATABASE, {
@@ -29,6 +30,7 @@ app.use(cors());
 
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
 
 // app.get("/", (req, res) => {
 //   res.send("Hey");
