483f283b5dff57882712a5180e27a44154f6fbee
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-03-28 11:06:12 +0530

    createCategory route

--- controllers/category.js
+++ controllers/category.js
@@ -3,11 +3,23 @@ const Category = require("../models/category");
 exports.getCategoryById = (req, res, next, id) => {
   Category.findById(id).exec((error, category) => {
     if (error) {
-      res.status(400).json({
+      return res.status(400).json({
         error: "category not found"
       });
     }
     req.category = category;
     next();
+  });
+};
+
+exports.createCategpry = (req, res) => {
+  const category = new Category(req.body);
+  category.save((error, category) => {
+    if (error) {
+      return res.status(400).json({
+        error: "Couldn't create categpry"
+      });
+    }
+    return res.json({ category });
   });
 };
--- routes/category.js
+++ routes/category.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const { getCategoryById } = require("../controllers/category");
+const { getCategoryById, createCategpry } = require("../controllers/category");
 const { isAdmin, isAuthenticated, isSignedIn } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
 
 router.param("userId", getUserById);
 router.param("categoryId", getCategoryById);
+
+router.post(
+  "/category/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  createCategpry
+);
 
 module.exports = router;
