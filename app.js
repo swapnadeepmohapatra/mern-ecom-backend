fe23a9b1663d9ca6fda3f36081884ab4baf12326
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-03-30 01:40:33 +0530

    changed port

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use("/api", orderRoutes);
 //   res.send("Hey");
 // });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
 });
