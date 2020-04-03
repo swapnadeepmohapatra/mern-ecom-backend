25df11f4ecc6148e44e2822bdd261a5bd99fb9d1
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-04-03 19:51:45 +0530

    resolved a very silly bug

--- controllers/product.js
+++ controllers/product.js
@@ -66,7 +66,7 @@ exports.getProduct = (req, res) => {
 exports.photo = (req, res, next) => {
   if (req.product.photo.data) {
     res.set("Content-Type", req.product.photo.contentType);
-    return res.send(req.product.product.data);
+    return res.send(req.product.photo.data);
   }
   next();
 };
