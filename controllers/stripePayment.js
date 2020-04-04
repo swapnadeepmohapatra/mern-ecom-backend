b7c1f7e0fe13044e05324b22ce8464210f1e7482
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-04-04 20:11:04 +0530

    fixed stripe payment bugs

--- controllers/stripePayment.js
+++ controllers/stripePayment.js
@@ -9,7 +9,7 @@ exports.makePayment = (req, res) => {
 
   let amount = 0;
   products.map(r => {
-    amount += r.price;
+    amount = amount + r.price;
   });
 
   return stripe.customers
@@ -18,28 +18,31 @@ exports.makePayment = (req, res) => {
       source: token.id
     })
     .then(customer => {
-      stripe.charges.create(
-        {
-          amount: amount * 100,
-          // currency: "usd",
-          currency: "inr",
-          customer: customer.id,
-          receipt_email: token.email,
-          description: `purchase of gadgets`,
-          shipping: {
-            name: token.card.name,
-            address: {
-              line1: token.card.address_line1,
-              line2: token.card.address_line2,
-              city: token.card.address_city,
-              country: token.card.address_country,
-              postal_code: token.card.address_zip
+      stripe.charges
+        .create(
+          {
+            amount: amount * 100,
+            currency: "usd",
+            customer: customer.id,
+            receipt_email: token.email,
+            description: `Purchased the product`,
+            shipping: {
+              name: token.card.name,
+              address: {
+                line1: token.card.address_line1,
+                line2: token.card.address_line2,
+                city: token.card.address_city,
+                country: token.card.address_country,
+                postal_code: token.card.address_zip
+              }
             }
+          },
+          {
+            idempotencyKey
           }
-        },
-        { idempontencyKey }
-      );
+        )
+        .then(result => res.status(200).json(result))
+        .catch(err => console.log(err));
     })
-    .then(result => res.status(200).json(result))
-    .catch(err => console.log(err));
+    .catch(console.log("FAILED"));
 };
